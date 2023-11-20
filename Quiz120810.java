6a5324a2a82d540eabd19f7885a61f594a2f10c3
YoonJongGu <124024784+YoonJongGu@users.noreply.github.com>
2023-11-21 02:46:20 +0900

    Lv0: 나머지 구하기

---
+++ Quiz120810.java
@@ -0,0 +1,19 @@
+package test.programmers;
+
+/*
+	나머지 구하기
+	
+	문제 설명
+	- 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+	
+	제한사항
+	- 0 < num1 ≤ 100
+	- 0 < num2 ≤ 100
+*/
+
+public class Quiz120810 {
+	public int solution(int num1, int num2) {
+        int answer = num1 % num2;
+        return answer;
+    }
+}
