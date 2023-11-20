348679502bb7966fdc2ffe2374d5ed2c49e78f2b
YoonJongGu <124024784+YoonJongGu@users.noreply.github.com>
2023-11-21 02:45:26 +0900

    Lv0: 두 수의 차

---
+++ Quiz120803.java
@@ -0,0 +1,19 @@
+package test.programmers;
+
+/*
+	두 수의 차
+	
+	문제 설명
+	- 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+	
+	제한사항
+	- -50000 ≤ num1 ≤ 50000
+	- -50000 ≤ num2 ≤ 50000
+*/
+
+public class Quiz120803 {
+	public int solution(int num1, int num2) {
+        int answer = num1 - num2;
+        return answer;
+    }
+}
